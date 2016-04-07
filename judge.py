5f95813a3905cb0bbcee6899086396c85930ba7f
riteme <1412803389@qq.com>
2016-04-07 18:35:55 +0800

    Fixed bug: memory display issue

--- judge.py
+++ judge.py
@@ -67,7 +67,7 @@ def memory_checker(pid, time_limit):
         try:
             for i in range(0, 5):
                 rss, vms = proc.memory_info()
-                usage = rss + vms
+                usage = rss
                 memory_max = max(memory_max, usage)
 
         except:
